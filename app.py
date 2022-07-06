a4e4f02c2b3b8a42f021d88862d4faefb15ba275
Ashish <ashish.patil401@gmail.com>
2022-07-07 02:34:57 +0530

    logger and exception module completed

--- app.py
+++ app.py
@@ -1,9 +1,19 @@
 from flask import Flask
+from housing.logger import logging
+from housing.exception import HousingException
+import sys
 
 app=Flask(__name__)
 
 @app.route("/", methods=['GET','POST'])
 def index():
+    try:
+        raise Exception("We are testing custom exception")
+    except Exception as e:
+        housing = HousingException(e,sys)
+        logging.info(housing.error_message)
+        logging.info("We are testing logging module")
+        
     return "CI/CD pipeline has been established..."
 
 
---
+++ housing/exception/__init__.py
@@ -0,0 +1,31 @@
+import os
+import sys
+
+class HousingException(Exception):
+
+    def __init__(self,error_message:Exception, error_details:sys):
+        super().__init__(error_message)   #line no. 7 is equivalent to Exception(error_message)
+
+        self.error_message = HousingException.get_detailed_error_message(error_message=error_message,
+                                                                         error_details=error_details)
+
+    
+    @staticmethod
+    def get_detailed_error_message(error_message:Exception, error_details:sys)->str:
+        """
+        error_message: Exception object
+        error_details: Object of sys module
+        """
+        _, _, exce_tb = error_details.exc_info()
+        line_number = exce_tb.tb_frame.f_lineno
+        file_name = exce_tb.tb_frame.f_code.co_filename
+        error_message = f"Error occured in script: [{file_name}] at line number [{line_number}] error message: [{error_message}] "    
+        return error_message
+
+
+    def __str__(self) -> str:
+        return self.error_message
+
+
+    def __repr__(self) -> str:
+        return HousingException.__name__.str()
